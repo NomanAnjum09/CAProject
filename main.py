3dd8390ba21efa51df19e84504e41c9c99a5ea8a
hassan11196 <hassan11196@hotmail.com>
2019-12-22 04:57:21 +0500

    Add GUI and Plotting and Pickling

--- main.py
+++ main.py
@@ -41,12 +41,16 @@ def observe_random(mem,program,r,c,no_of_sets,assoc):
         print("checking memory")
         print(init_cache_row,end=" ")
         print(final_cache_row)
+        break_outer = False
         for i in range (init_cache_row,final_cache_row+1):
+            if break_outer:
+                break
             for j in range(c):
                 print(mem[i,j],end=" ")
                 if(mem[i,j]==address):
                     hit=hit+1
                     flag=1
+                    break_outer = True
                     break 
         print("mem check complete")
         if(flag==1):
@@ -66,7 +70,7 @@ def observe_random(mem,program,r,c,no_of_sets,assoc):
 
 
 
-def observe_lor(mem,mod1,mod2,r,c,no_of_sets,assoc):
+def observe_lor(mem,mod1,mod2,r,c,no_of_sets,assoc,mem_accesses):
     staticsize=10
     hit=0
     miss=0
@@ -75,7 +79,7 @@ def observe_lor(mem,mod1,mod2,r,c,no_of_sets,assoc):
     print(mod2)
     print(mem)
     program=[mod1,mod2]
-    for i in range(1000):
+    for i in range(mem_accesses):
         module=random.randint(0,1)
         
         address=random.randint(program[module][0,0],program[module][staticsize-1,c-1])#accessing an address from module  
@@ -117,57 +121,62 @@ def observe_lor(mem,mod1,mod2,r,c,no_of_sets,assoc):
             mem[victim,i]=program[module][module_row,i]
     print(hit)
     print(miss)
+    return {'hit_rate':hit/1000, 'miss_rate':miss/1000, 'hit_count':hit, 'miss_count' : miss}
+
+def driver(csize, sets, bsize, mem_accesses):
+    
+
+    csize=int(csize)
+    sets=int(sets)
+    bsize=int(bsize)
+
+
+    no_of_blocks=int(csize)/bsize
+    no_of_blocks=int(no_of_blocks)
+    no_of_sets=no_of_blocks/sets
+    tem=csize
+
+    max_array = no_of_blocks
+    mem = np.arange(max_array*bsize).reshape((max_array,bsize))
+    for i in range(max_array):
+        for j in range(bsize):
+            mem[i,j]=-1
+    fill_random(max_array,bsize,mem)
+
+    #program is divided into 3 module so that temporal and spatial locality can be observed
+
+    module_size1=10*bsize
+    module_size2=10*bsize
+    module_size3=10*bsize
+    module_size5=10*bsize
+    module_size4=10*bsize
+
+    module1= np.arange(module_size1).reshape((10,bsize))
+    module2= np.arange(module_size2).reshape((10,bsize))
+    module3= np.arange(module_size3).reshape((10,bsize))
+    module4= np.arange(module_size4).reshape((10,bsize))
+    module5= np.arange(module_size5).reshape((10,bsize))
 
 
 
-
-csize=input("Enter Size Of Cache ")
-sets =input("Enter Set Associativity ")  
-bsize = input("Enter Size Of Block " )
-
-csize=int(csize)
-sets=int(sets)
-bsize=int(bsize)
-
-
-no_of_blocks=int(csize)/bsize
-no_of_blocks=int(no_of_blocks)
-no_of_sets=no_of_blocks/sets
-tem=csize
-
-max_array = no_of_blocks
-mem = np.arange(max_array*bsize).reshape((max_array,bsize))
-for i in range(max_array):
-    for j in range(bsize):
-        mem[i,j]=-1
-fill_random(max_array,bsize,mem)
-
-#program is divided into 3 module so that temporal and spatial locality can be observed
-
-module_size1=10*bsize
-module_size2=10*bsize
-module_size3=10*bsize
-module_size5=10*bsize
-module_size4=10*bsize
-
-module1= np.arange(module_size1).reshape((10,bsize))
-module2= np.arange(module_size2).reshape((10,bsize))
-module3= np.arange(module_size3).reshape((10,bsize))
-module4= np.arange(module_size4).reshape((10,bsize))
-module5= np.arange(module_size5).reshape((10,bsize))
-
-
-
-program=[module1,module2,module3,module4,module5]
-global number
-number=0
-for i in range(5):
-    fill_serial(max_array,bsize,program[i])
-n1=random.randint(0,4)
-n2=random.randint(0,4)
-while(n2==n1):
+    program=[module1,module2,module3,module4,module5]
+    global number
+    number=0
+    for i in range(5):
+        fill_serial(max_array,bsize,program[i])
+    n1=random.randint(0,4)
     n2=random.randint(0,4)
-observe_lor(mem,program[n1],program[n2],max_array,bsize,no_of_sets,sets)
-#observe_random(mem,program,max_array,bsize,no_of_sets,sets)
+    while(n2==n1):
+        n2=random.randint(0,4)
+    output = observe_lor(mem,program[n1],program[n2],max_array,bsize,no_of_sets,sets, mem_accesses)
+    #observe_random(mem,program,max_array,bsize,no_of_sets,sets)
 
-print(program)
+    print(program)
+    return output
+
+if __name__ == "__main__":
+    csize=input("Enter Size Of Cache ")
+    sets =input("Enter Set Associativity ")  
+    bsize = input("Enter Size Of Block " )
+    driver(csize, sets, bsize)
+    
